4dacc1fe7aba8b5e4a7a4a73bada54128f1ad062
Co-Pierre Georg <cogeorg@protonmail.com>
2024-02-16 14:22:52 +0100

    Before new model iteration

--- 90_compute_equilibrium.py
+++ 90_compute_equilibrium.py
@@ -18,27 +18,27 @@ import networkx as nx
 # -------------------------------------------------------------------------
 # do_run(file_name)
 # -------------------------------------------------------------------------
-def do_run(base_directory, dependency_identifier, covariate_identifier, delta):
+def do_run(base_directory, dependency_identifier, covariate_identifier, delta, col_num):
     edge_filename = base_directory + dependency_identifier + ".edgelist"
     covariate_filename = base_directory + covariate_identifier + ".csv"
 
     output_filename = base_directory + "equilibria_" + dependency_identifier + ".csv"
 
     print(str(datetime.datetime.now()) + " <<<< WORKING")
-    print(str(datetime.datetime.now()) + "  DEPENDENCIES: " + dependency_identifier)
-    print(str(datetime.datetime.now()) + "  COVARIATES: " + covariate_identifier)
+    print(str(datetime.datetime.now()) + "  DEPENDENCIES: " + edge_filename)
+    print(str(datetime.datetime.now()) + "  COVARIATES: " + covariate_filename)
     
     # TO USE THE NETWORK STORED IN THE EDGE_FILENAME, USE THE BELOW:
-    # G = nx.read_edgelist(edge_filename, create_using=nx.DiGraph())
+    G = nx.read_edgelist(edge_filename, create_using=nx.DiGraph())
 
     # OTHERWISE, USE UNTIL >>> TO MANUALLY ADD EDGES TO AN EMPTY GRAPH
     # <<<
-    G = nx.create_empty_copy(
-        nx.read_edgelist(edge_filename, create_using=nx.DiGraph())                     
-                             )
-    print(G.nodes())
-    G.add_edges_from([('0','1')])
-    G.add_edges_from([('1','2')])
+    # G = nx.create_empty_copy(
+    #     nx.read_edgelist(edge_filename, create_using=nx.DiGraph())                     
+    #                          )
+    # print(G.nodes())
+    # G.add_edges_from([('0','1')])
+    # G.add_edges_from([('1','2')])
     # G.add_edges_from([('0','2')])
     # >>>
 
@@ -46,11 +46,14 @@ def do_run(base_directory, dependency_identifier, covariate_identifier, delta):
     num_nodes = G.number_of_nodes()
     num_edges = G.number_of_edges()
     
+    print(str(datetime.datetime.now()) + "  DELTA: " + str(delta))
     print(str(datetime.datetime.now()) + "  # NODES: " + str(num_nodes) + " # EDGES: " + str(num_edges))
 
     # CREATE DATA
     covariate_data = np.genfromtxt(covariate_filename, delimiter=';', skip_header=1, dtype=float)
-    theta = covariate_data[:,4]/np.sum(covariate_data[:,4])
+    theta = covariate_data[:,col_num] # normalized in stata
+    # theta = (1.0/num_nodes)*np.ones(num_nodes)  # only for debugging purposes
+        
     Gm = nx.to_numpy_array(G)
     
     I = np.eye(num_nodes)
@@ -58,7 +61,7 @@ def do_run(base_directory, dependency_identifier, covariate_identifier, delta):
 
     inv_mat = np.linalg.inv(I - delta*Gm)
 
-    if True:
+    if False:
         print(Gm, theta)
         print(inv_mat)
     #
@@ -70,19 +73,19 @@ def do_run(base_directory, dependency_identifier, covariate_identifier, delta):
     print(str(datetime.datetime.now()) + " << FINISHED EQUILIBRIUM COMPUTATION")
 
     # COMPUTE WELFARE
-    W_eq = theta @ (One - p_eq) + 0.5*(One - q_eq) @ (One - q_eq)
+    W_eq = theta @ (One - p_eq) - 0.5*(One - q_eq) @ (One - q_eq)
     print(str(datetime.datetime.now()) + "   W_eq =", W_eq)
 
     #
     # SOCIAL OPTIMUM CASE 
     #
     # COMPUTE SOCIAL OPTIMUM
-    q_so = One - (inv_mat @ theta) # checked and correct
+    q_so = One - (np.linalg.inv(I - delta*np.transpose(Gm)) @ theta) # checked and correct
     p_so = inv_mat @ (One - np.linalg.inv(I - delta*np.transpose(Gm)) @ theta )  # checked and correct
     print(str(datetime.datetime.now()) + " << FINISHED SOCIAL OPTIMUM COMPUTATION")
 
     # COMPUTE WELFARE
-    W_so = theta @ (One - p_so) + 0.5*(One - q_so) @ (One - q_so)
+    W_so = theta @ (One - p_so) - 0.5*(One - q_so) @ (One - q_so)
     print(str(datetime.datetime.now()) + "   W_so =", W_so)
 
     #
@@ -94,7 +97,7 @@ def do_run(base_directory, dependency_identifier, covariate_identifier, delta):
     for i in range(0,num_nodes):
         out_text += str(i) + " " +str(q_eq[i]) + " " + str(p_eq[i]) + " " + str(q_so[i]) + " " + str(p_so[i]) + "\n"
     
-    print(out_text)
+    # print(out_text)
     out_file.write(out_text)
     out_file.close()
     print("  >> FILE WRITTEN TO:" + output_filename)
@@ -117,8 +120,9 @@ if __name__ == '__main__':
     dependency_identifier = args[2]
     covariate_identifier = args[3]
     delta = float(args[4])
+    col_num = int(args[5])
     
 #
 # CODE
 #
-    do_run(base_directory, dependency_identifier, covariate_identifier, delta)
+    do_run(base_directory, dependency_identifier, covariate_identifier, delta, col_num)
